096ffe80eb74af0bdeae43e8d1e542916724890a
3ouri <abdallah_ouri@hotmail.com>
2014-06-11 15:51:35 +0300

    Update temp.js

--- www/temp.js
+++ www/temp.js
@@ -1,5 +1,5 @@
 var myPlugin = {
-    runPlugin: function(successCallback, errorCallback, ) {
+    runPlugin: function(successCallback, errorCallback) {
         cordova.exec(
     successCallback, // success callback function
     errorCallback, // error callback function
